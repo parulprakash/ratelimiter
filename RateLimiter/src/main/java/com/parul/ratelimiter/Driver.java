574f54b328b886878a8a5580273c078c45b83d5d
parulprakash <parulprakash26@gmail.com>
2019-12-26 10:14:12 +0530

    Update Driver.java

--- RateLimiter/src/main/java/com/parul/ratelimiter/Driver.java
+++ RateLimiter/src/main/java/com/parul/ratelimiter/Driver.java
@@ -6,34 +6,27 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    static RateLimiter rateLimiter = new RateLimiterImpl(3, new CustomTimeUnit(TimeUnit.SECONDS, 10));
+    static RateLimiter rateLimiter = new RateLimiterImpl(2, new CustomTimeUnit(TimeUnit.SECONDS, 5));
     static List<User> users = new ArrayList<User>();
     public static void main(String[] args) {
-
-
-        for(int i = 0; i < 5; ++i) {
-
-            User user = new User(i);
+         for(int i = 0; i < 5; ++i) {
+         User user = new User(i);
             users.add(user);
-
-        }
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-        System.out.println(callFunction());
-
+         }
+        callFunction(users.get(0));
+        callFunction(users.get(0));
+        callFunction(users.get(0));
+        callFunction(users.get(1));
+        callFunction(users.get(1));
+        callFunction(users.get(1));
         try {
             Thread.sleep(5000);
-            System.out.println(callFunction());
-            System.out.println(callFunction());
-            Thread.sleep(5000);
-            System.out.println(callFunction());
-            System.out.println(callFunction());
-            System.out.println(callFunction());
+            callFunction(users.get(0));
+            callFunction(users.get(1));
+            callFunction(users.get(0));
+            callFunction(users.get(1));
+            callFunction(users.get(0));
+            callFunction(users.get(1));
         } catch (InterruptedException e ) {
 
         }
@@ -42,10 +35,14 @@ public class Driver {
 
     }
 
-    public static boolean callFunction(){
-
+    public static void callFunction(User user){
         System.out.println(new Date());
-        return ((RateLimiterImpl)rateLimiter).isRequestAllowed(users.get(0));
+        try {
+            (rateLimiter).isRequestAllowed(user);
+            System.out.println("Successfully requested");
+        } catch(RateLimiterException e) {
+            System.out.println(e);
+        }
 
     }
 }
